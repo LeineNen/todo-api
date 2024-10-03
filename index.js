595de71ff0c892df60311a789b6c28df0bcc4231
Mary Mag <maymag4on@gmail.com>
2024-10-03 15:29:52 +0000

    added cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import todoRouter from './routes/todo.routes.js';
 import userRouter from './routes/user.routes.js';
+import cors from 'cors'
 
 // connect to database
 await mongoose.connect(process.env.MONGO_URI);
@@ -11,6 +12,7 @@ await mongoose.connect(process.env.MONGO_URI);
 const app = express();
 
 // use middlewares
+app.use(cors())
 app.use(express.json())
 
 //  Use routes
